593a932cda8977181341506d7a325052146179b0
Geoffrey <geoffreyplichard@gmail.com>
2015-07-16 12:16:27 +0200

    Add account creation activity and fields

--- app/src/main/java/com/myschool/tp2/SignupActivity.java
+++ app/src/main/java/com/myschool/tp2/SignupActivity.java
@@ -1,35 +1,23 @@
 package com.myschool.tp2;
 
 import android.support.v7.app.ActionBarActivity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
 
 
-public class MainActivity extends ActionBarActivity {
-
-    private Button btn_signup = null;
-    private Button btn_signin = null;
+public class SignupActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        btn_signup = (Button) findViewById(R.id.btn_signup);
-        btn_signin = (Button) findViewById(R.id.btn_signin);
-
-        btn_signup.setOnClickListener(goSignupListener);
+        setContentView(R.layout.activity_signup);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_signup, menu);
         return true;
     }
 
@@ -47,15 +35,4 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
-
-    private OnClickListener goSignupListener = new OnClickListener() {
-        @Override
-        public void onClick(View v) {
-
-            Intent signupActivity = new Intent(MainActivity.this, SignupActivity.class);
-
-            startActivity(signupActivity);
-
-        }
-    };
 }
